67446885e0b4d13dc79962bd78c3ff1ed2903ef0
[faith okpala] <[okpalafaith1@gmail.com]>
2020-01-09 15:46:27 +0100

    done with my database , skill and nav section

---
+++ public/index.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+    $("#contact").on('submit', function(e) {
+        e.preventDefault();
+        let name = $('#name').val();
+        let email = $('#email').val();
+        let message = $('#message').val();
+
+        $.ajax({
+            url: "http://localhost:3000/portfolio",
+
+            success: function(result) {
+                location.reload()
+
+            },
+            method: "POST",
+            data: {
+                name,
+                email,
+                message
+            }
+        });
+
+    })
+})
